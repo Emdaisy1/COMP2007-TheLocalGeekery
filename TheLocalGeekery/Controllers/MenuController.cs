38df7b89519344f385e683a28ce9ea79eecd88a2
Emdaisy1 <hilborn_emmav@live.ca>
2016-07-30 23:44:19 -0400

    Starting as new project in an attempt to fix syncing & deployment issues.

---
+++ TheLocalGeekery/Controllers/MenuController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TheLocalGeekery.Models;
+
+namespace TheLocalGeekery.Controllers
+{
+    public class MenuController : Controller
+    {
+        private MenuItemEntities menuDB = new MenuItemEntities();
+
+        // GET: StoreManager
+        public async Task<ActionResult> Index()
+        {
+            var menuItems = menuDB.MenuItems;
+            return View(await menuItems.ToListAsync());
+        }
+
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MenuItem menuItem = await menuDB.MenuItems.FindAsync(id);
+            if (menuItem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(menuItem);
+        }
+    }
+}
